fea908f45c229ca45c80fc9103ac55f885590a55
AleRMM <karen.ramirez.18@hotmail.com>
2023-02-21 17:55:28 -0600

    feat: user schema and auth whith jwt

--- src/graphql/resolvers/Mutations.js
+++ src/graphql/resolvers/Mutations.js
@@ -1,32 +1,30 @@
-import Movie from "../../models/Movie.js";
 import User from "../../models/User.js";
+import createToken from "./Auth.js"
 
 const Mutation = {
 
-//CREATE
-    async createMovie(_, 
-        {title, original_title, content, director, date_of_realease, duration,
-        category, picture_url, trailer_url, likes}) {
+//REGISTER USER
+    async register(_, {username, email, password}) {
 
-            const newMovie = {title, original_title, content, director, 
-                date_of_realease, duration, category, picture_url, trailer_url, likes};
-
-            const movie = await Movie.create(newMovie);
-            return movie
-        },
-//UPDATE
-    async updateMovie(_, 
-        {_id, title, original_title, content, director, date_of_realease, duration,
-        category, picture_url, trailer_url, likes}) {
-
-        const movie = {title, original_title, content, director, 
-            date_of_realease, duration, category, picture_url, trailer_url, likes};
-        return await Movie.findByIdAndUpdate(_id, movie, {new: true})
+        const newUser = {username, email, password};
+        const user = await User.create(newUser);
+        //JWT
+        const token = createToken(user)
+        return token
     },
-//DELETE
-    async deleteMovie(_, {_id}) {
-        await Movie.findByIdAndDelete(_id)
-        return await Movie.find()
+
+    async login(_, {email, password}) {
+
+        const args = {email, password};
+
+        const user = await User.findOne({email: args.email}).select('+password')
+
+        if(!user || args.password !== user.password) 
+            throw new Error("Invalid credentials")
+
+        const token = createToken(user)
+
+        return token
     }
 }
 
--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,7 +1,5 @@
-import Query from "./Query.js";
 import Mutation from "./Mutations.js";
 
 export default {
-    Query,
     Mutation
 }
